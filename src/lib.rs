6c08d20b3f8d2ac3408aa1e3048b7549348725da
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-13 09:25:34 +0200

    Resolved issues regarding README

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
 #![doc = include_str!("../README.md")]
 
-#[cfg(feature = "diesel")]
-#[macro_use]
-extern crate diesel;
-
 #[cfg(feature = "schemars")]
 #[macro_use]
 extern crate schemars;
 
 pub mod errors;
 mod ewkb;
-#[cfg(feature = "diesel")]
 pub mod functions;
-#[cfg(feature = "diesel")]
 pub mod functions_nullable;
 #[cfg(feature = "serde_geojson")]
 mod geojson;
@@ -22,7 +16,6 @@ mod linestring;
 mod multiline;
 mod multipoint;
 mod multipolygon;
-#[cfg(feature = "diesel")]
 pub mod operators;
 mod points;
 mod polygon;
